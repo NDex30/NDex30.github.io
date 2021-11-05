70e323b7bc6e67a939d7d962995149aa233f1949
NDex30 <oudex30@gmail.com>
2021-11-04 23:00:27 -0400

    adding yearncash project

---
+++ projects/yearncash/_next/static/J_uUYnsJPualjvt2_MMn6/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Froadmap","\u002Fteam","\u002Fwallet","\u002Fwhitepaper","\u002Fcodeofconduct","\u002Finvest","\u002Fcodeofethics","\u002F"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
